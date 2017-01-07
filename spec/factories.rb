a03c65669be5b41f3b1b739c4341d4d15b5828a4
David Kreuzburg <dmk1991@gmail.com>
2017-01-07 02:46:14 +0000

    Adds classes for STI.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,19 @@
 FactoryGirl.define do
+  factory :knight do
+    
+  end
+  factory :bishop do
+    
+  end
+  factory :rook do
+    
+  end
+  factory :king do
+    
+  end
+  factory :queen do
+    
+  end
   factory :piece do
 
   end
