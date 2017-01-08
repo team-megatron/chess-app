9f8f324aacb223070ad32419846208a13f7c9b1e
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-01-08 11:22:36 +0000

    Add top navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :players
-  root 'static_pages#index'
+  #root 'static_pages#index'
+  root 'games#index'
+  resources :games
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
