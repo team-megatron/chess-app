603c6f72aea18d54e2844d5df43312efda8bdddb
David Kreuzburg <dmk1991@gmail.com>
2017-01-09 02:26:52 +0000

    Removes unnecessary factories.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,27 +1,14 @@
 FactoryGirl.define do
-  factory :knight do
-    
-  end
-  factory :bishop do
-    
-  end
-  factory :rook do
-    
-  end
-  factory :king do
-    
-  end
-  factory :queen do
-    
-  end
   factory :piece do
-    
+
   end
+
   factory :move do
-    
+
   end
+
   factory :game do
-    
+
   end
   factory :user do
     sequence :email do |n|
