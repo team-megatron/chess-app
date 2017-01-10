52a3950f6fdbd15e3b35367276b2e167a933d692
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-01-10 11:08:10 +0000

    List all games and button to join

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,26 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def white_player_name(game)
+    if game.white_player.present?
+      game.white_player.name
+    else
+      nil
+    end
+  end
+
+  def black_player_name(game)
+    if game.black_player.present?
+      game.black_player.name
+    else
+      nil
+    end
+  end
+
+  def isOpen?(game)
+    game.black_player.blank?
+  end
+
+  helper_method :white_player_name, :black_player_name, :isOpen?
 end
