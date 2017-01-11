873ab35e3a01e0f019304c0ccb961ed813107f2c
Ian Pan <ianpan@hotmail.com>
2017-01-11 01:59:04 +0000

    is_obstructed_horizontally

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,54 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
+
+  def is_obstructed?(row_destination, col_destination)
+    if is_moving_horizontal?(row_destination)
+      is_obstructed_horizontally?(col_destination)
+    end
+  end
+
+  def is_moving_horizontal?(row_destination)
+    if self.row == row_destination
+      return true
+    end
+  end
+
+  def is_obstructed_horizontally?(col_destination)
+    #steps array is the location between piece and destination
+    steps = []
+    #find the spots in between piece and destination
+    if self.column < col_destination
+      ((self.column + 1)...col_destination).each do |col|
+        steps << col
+      end
+    else
+      ((col_destination + 1)...self.column).each do |col|
+        steps << col
+      end
+    end
+  
+    
+    pieces = self.game.pieces
+    pieces.each do |piece|
+      steps.each do |step|
+        if self.row == piece.row && piece.column == step
+          return true
+        end
+      end
+    end
+    
+    return false
+  end
+
+  def is_moving_vertical?(col_destination)
+    if self.column == col_destination
+      return true
+    end
+  end
+
+  def is_obstructed_diagonally?(row_destination, col_destination)
+    steps = {}
+    
+  end
+
 end
