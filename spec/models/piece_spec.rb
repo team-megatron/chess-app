0f5ca603e02cdea9b528ab05eeb15510fc67d082
Ian Pan <ianpan@hotmail.com>
2017-01-12 06:34:50 +0000

    make tests wording more consistent but found an error

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -102,7 +102,7 @@ RSpec.describe Piece, type: :model do
   describe "is_obstructed_diagonally?" do
     it "should return true if obstructed moving up and right" do
       game = FactoryGirl.create(:game)
-      moving_piece = FactoryGirl.create(:piece, column: 1, game_id: game.id)
+      moving_piece = FactoryGirl.create(:piece, row: 1, column: 1, game_id: game.id)
       obstructing_piece = FactoryGirl.create(:piece, row: 4, column: 4, game_id: game.id)
 
       expect(moving_piece.is_obstructed_diagonally?(5,5)).to eq true
@@ -110,34 +110,34 @@ RSpec.describe Piece, type: :model do
 
     it "should return true if obstructed moving down and right" do
       game = FactoryGirl.create(:game)
-      moving_piece = FactoryGirl.create(:piece, column: 1, row: 7, game_id: game.id)
-      obstructing_piece = FactoryGirl.create(:piece, column: 3, row: 5, game_id: game.id)
+      moving_piece = FactoryGirl.create(:piece, row: 7, column: 1, game_id: game.id)
+      obstructing_piece = FactoryGirl.create(:piece, row: 5, column: 3, game_id: game.id)
 
       expect(moving_piece.is_obstructed_diagonally?(4,4)).to eq true
     end
 
     it "should return true if obstructed moving up and left" do
       game = FactoryGirl.create(:game)
-      moving_piece = FactoryGirl.create(:piece, column: 3, game_id: game.id)
-      obstructing_piece = FactoryGirl.create(:piece, column: 2, row: 2, game_id: game.id)
+      moving_piece = FactoryGirl.create(:piece, row: 1, column: 3, game_id: game.id)
+      obstructing_piece = FactoryGirl.create(:piece, row: 2, column: 2, game_id: game.id)
 
       expect(moving_piece.is_obstructed_diagonally?(3,1)).to eq true
     end
 
     it "should return true if obstructed moving down and left" do
       game = FactoryGirl.create(:game)
-      moving_piece = FactoryGirl.create(:piece, column: 8, row: 8, game_id: game.id)
-      obstructing_piece = FactoryGirl.create(:piece, column: 7, row: 7, game_id: game.id)
+      moving_piece = FactoryGirl.create(:piece, row: 6, column: 8, game_id: game.id)
+      obstructing_piece = FactoryGirl.create(:piece, row: 4, column: 6, game_id: game.id)
 
-      expect(moving_piece.is_obstructed_diagonally?(6,6)).to eq true
+      expect(moving_piece.is_obstructed_diagonally?(1,3)).to eq true
     end
 
     it "should return false if not obstructed diagonally" do
       game = FactoryGirl.create(:game)
-      moving_piece = FactoryGirl.create(:piece, column: 8, row: 8, game_id: game.id)
-      obstructing_piece = FactoryGirl.create(:piece, column: 5, row: 4, game_id: game.id)
+      moving_piece = FactoryGirl.create(:piece, row: 8, column: 8, game_id: game.id)
+      obstructing_piece = FactoryGirl.create(:piece, row: 2, column: 2, game_id: game.id)
 
-      expect(moving_piece.is_obstructed_diagonally?(6,6)).to eq false
+      expect(moving_piece.is_obstructed_diagonally?(3,3)).to eq false
     end
   end
 end
