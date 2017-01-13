40a801a79f3640484daff8f1e5eb438acce784d7
David Kreuzburg <dmk1991@gmail.com>
2017-01-13 03:32:26 +0000

    Moves horizontal check into DB query.

--- app/models/helpers/piece_helper.rb
+++ app/models/helpers/piece_helper.rb
@@ -4,24 +4,15 @@ module PieceHelper
       return false
     end
 
-    # Pull in all pieces from same game and same row
-    pieces = self.game.pieces.where(row: self.row)
+    incrementing = self.column < col_destination
+    start_column = self.column + (incrementing ? 1 : -1)
+    end_column = col_destination + (incrementing ? -1 : 1)
 
-    # Determine if pieces is moving right or left.
-    col_increment = self.column < col_destination ? 1 : -1
-    next_column = self.column + col_increment
-
-    # Loop through all columns between start and end position.
-    # Return true if any pieces on the row exist at specified column.
-    # (* col_increment) is done to allow < to always be correct operand.
-    while (next_column * col_increment) < (col_destination * col_increment)
-      pieces.each do |piece|
-        return true if piece.column == next_column
-      end
-      next_column += col_increment
+    if incrementing
+      self.game.pieces.where(row: self.row, column: (start_column..end_column)).exists?
+    else
+      self.game.pieces.where(row: self.row, column: (end_column..start_column)).exists?
     end
-
-    return false
   end
 
   def is_obstructed_vertically?(row_destination, col_destination)
