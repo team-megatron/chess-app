94219ccc459619a580d3319a2397d7eda86e3021
David Kreuzburg <dmk1991@gmail.com>
2017-01-14 23:44:07 +0000

    Starts populate method. Builds tests.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,10 @@ class Game < ActiveRecord::Base
   has_many :pieces
   belongs_to :white_player, class_name: 'Player'
   belongs_to :black_player, class_name: 'Player'
+
+  def populate_game!
+    8.times do |n|
+      Pawn.create(game_id: self.id, row: 2, column: n)
+    end
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  describe "populate_game!" do
+    it "should create all necessary pieces for a game of chess" do
+      game = FactoryGirl.create(:game)
+      game.populate_game!
+      pieces = game.pieces
+      expect(pieces.length).to eq 32
+    end
+  end
 end
