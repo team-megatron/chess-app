5f008e058cab6b5306c014d523815ab674b311a7
David Kreuzburg <dmk1991@gmail.com>
2017-01-17 20:15:29 +0000

    Cleans up repetitive code.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,10 +1,14 @@
 module GamesHelper
-  def select_html_symbol(piece_type, color)
+  def select_html_symbol(piece)
     white_symbols = {'Rook'=> '&#9814;', 'Knight'=>'&#9816;', 'Bishop'=>'&#9815;',
                     'Queen'=> '&#9813;', 'King'  =>'&#9812;', 'Pawn'  =>'&#9817;'}
     black_symbols = {'Rook'=>'&#9820;', 'Knight' =>'&#9822;', 'Bishop'=>'&#9821;',
                     'Queen'=> '&#9819;', 'King'  =>'&#9818;', 'Pawn'  =>'&#9823;'}
 
-    color == 'black' ? black_symbols[piece_type] : white_symbols[piece_type]
+    if piece
+      return piece.is_black? ? black_symbols[piece.type] : white_symbols[piece.type]
+    else
+      return ''
+    end
   end
 end
