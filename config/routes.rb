498d17e0d4c956c67f65133553203d1b7ba355ef
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-01-18 11:49:33 +0000

    Add pieces controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :players
   root 'static_pages#index'
-  resources :games
+  resources :games, :pieces
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
