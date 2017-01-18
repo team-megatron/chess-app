b1c7cf669520a2dc720219f3ab83008e8c5ec621
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-01-18 13:56:56 +0000

    Add select and deselect a piece

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,17 @@
 class PiecesController < ApplicationController
+
+  def show
+    # if no piece is selected before, update current piece's isSelected
+    # else if the same piece has been selected before, deselect it
+
+    piece = Piece.find(params[:id])
+    piecesSelected = Piece.where(isSelected: true)
+    if piecesSelected.empty?
+      piece.update_attributes(isSelected: true)
+    elsif piecesSelected[0].id == piece.id
+      piece.update_attributes(isSelected: false)
+    end
+    redirect_to game_path(piece.game)
+  end
+
 end
