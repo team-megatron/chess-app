7c78f9fc6b08b0bae871c265071b2c772f4d2b62
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-01-19 12:27:48 +0000

    Piece position update and new move now in pieces_controller

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,29 +1,2 @@
 class MovesController < ApplicationController
-  def create
-    # TODO: check the move and any piece to be captured.
-    # now assume the move is valid and the target square is empty,
-    # move the piece and deselect it.
-    pieces_selected = Piece.where(game_id: params[:game_id], is_selected: true)
-    if !pieces_selected.empty?
-      piece = pieces_selected[0]
-
-      # update only when the position changes
-      if piece.row != params[:row] && piece.column != params[:column]
-        # TODO: improve this simple move recording.
-        move_string = "#{piece.type},"\
-                      "r#{piece.row},c#{piece.column},"\
-                      "r#{params[:row]},c#{params[:column]}"
-        @move = Move.create(game_id: params[:game_id],
-                            is_black: params[:is_black],
-                            move_string: move_string)
-        # update piece position
-        piece.update_attributes(column: params[:column], row: params[:row])
-      end
-      # deselect the piece
-      piece.update_attributes(is_selected: false)
-      # refresh the chessboard
-      redirect_to game_path(piece.game)
-    end
-  end
-
 end
