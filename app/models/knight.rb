ef3b6cb3652bcbc98290760e9d132d17cacbdd78
David Kreuzburg <dmk1991@gmail.com>
2017-01-21 16:08:11 +0000

    Implements knight valid_move? method.

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,8 @@
 class Knight < Piece
+  def valid_move?(row_destination, col_destination)
+    row_diff = (self.row - row_destination).abs
+    col_diff = (self.column - col_destination).abs
+
+    (row_diff / col_diff) == 2 || (col_diff / row_diff) == 2 ? true : false
+  end
 end
