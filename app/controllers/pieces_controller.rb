de6940ae53fef6b0fa6970da9cd9eeab0c35d97e
David Kreuzburg <dmk1991@gmail.com>
2017-01-21 03:19:22 +0000

    Moves moving into model method.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,42 +1,35 @@
 class PiecesController < ApplicationController
+  before_action :current_piece, only: [:show, :update]
 
   def show
     # if no piece is selected before, update current piece's is_selected
     # else if the same piece has been selected before, deselect it
 
-    piece = Piece.find(params[:id])
     pieces_selected = Piece.where(is_selected: true, game_id: params[:game_id])
     if pieces_selected.empty?
-      piece.update_attributes(is_selected: true)
+      current_piece.update_attributes(is_selected: true)
     elsif pieces_selected[0].id == piece.id
-      piece.update_attributes(is_selected: false)
+      current_piece.update_attributes(is_selected: false)
     end
-    redirect_to game_path(piece.game)
+    redirect_to game_path(current_piece.game)
   end
 
   def update
-    # TODO: check the move and any piece to be captured.
-    # now assume the move is valid and the target square is empty,
-    # move the piece and deselect it.
-    piece = Piece.find(params[:id])
-    if piece
-      # update only when the position changes
-      if piece.row != params[:row] && piece.column != params[:column]
-        # TODO: improve this simple move recording.
-        move_string = "#{piece.type},"\
-                      "r#{piece.row},c#{piece.column},"\
-                      "r#{params[:row]},c#{params[:column]}"
-        @move = Move.create(game_id: params[:game_id],
-                            is_black: params[:is_black],
-                            move_string: move_string)
-        # update piece position
-        piece.update_attributes(column: params[:column], row: params[:row])
-      end
-      # deselect the piece
-      piece.update_attributes(is_selected: false)
-      # refresh the chessboard
-      redirect_to game_path(piece.game)
+    if is_actual_move?(params[:row], params[:column])
+      current_piece.move_to(params[:row], params[:column])
+      redirect_to game_path(current_piece.game)
+    else
+      redirect_to game_path(current_piece.game)
     end
   end
 
+  private
+
+    def current_piece
+      @piece ||= Piece.find(params[:id])
+    end
+
+    def is_actual_move?(row_destination, col_destination)
+      current_piece.row != row_destination && current_piece.column != col_destination
+    end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -10,4 +10,10 @@ class Piece < ActiveRecord::Base
     return true if is_obstructed_diagonally?(row_destination, col_destination)
     return false
   end
+
+  def move_to(row_destination, col_destination)
+    # update piece position and deselect the piece
+    self.record_move(row_destination, col_destination)
+    self.update_attributes(column: col_destination, row: row_destination, is_selected: false)
+  end
 end
