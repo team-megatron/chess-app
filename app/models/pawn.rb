27a0038fb4c52483debd26a1f532f846c18aa2f3
David Kreuzburg <dmk1991@gmail.com>
2017-01-21 23:37:24 +0000

    Starts implementation of pawn valid move check.

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,5 +1,12 @@
 class Pawn < Piece
   def valid_move?(row_destination, col_destination)
+    start_row = self.is_black? ? 7 : 2
+    direction = self.is_black ? 1 : -1
 
+    if self.row == start_row
+      return (self.row - row_destination) <= 2 * direction
+    else
+      return (self.row - row_destination) == 1 * direction
+    end
   end
 end
