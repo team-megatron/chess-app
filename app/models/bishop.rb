58f6ef732999f908fb8b231a057c904e18140ba3
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-01-22 13:46:05 +0000

    Add bishop's valid_move? and Rspec tests

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,21 @@
 class Bishop < Piece
+  include PieceHelper
+
+  def valid_move?(row_dest_int, column_dest_int)
+    if row_dest_int < 1 || row_dest_int > 8 ||
+       column_dest_int < 1 || column_dest_int > 8
+      return false
+    end
+
+    col_diff = self.column - column_dest_int
+    row_diff = self.row - row_dest_int
+    # invalid if not on diagonal lines or obstructed diagonally
+    if col_diff.abs != row_diff.abs ||
+       is_obstructed_diagonally?(row_dest_int, column_dest_int)
+      return false
+    end
+
+    return true
+  end
+
 end
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Bishop, type: :model do
+  describe 'valid_move?' do
+    it "should should return true when moving diagonally" do
+      game = FactoryGirl.create(:game)
+      bishop = FactoryGirl.create(:bishop, row: 1, column: 3, game_id: game.id)
+
+      expect(bishop.valid_move?(4, 6)).to eq true
+      expect(bishop.valid_move?(2, 4)).to eq true
+    end
+
+    it "should return false when moving horizontal" do
+      game = FactoryGirl.create(:game)
+      bishop = FactoryGirl.create(:bishop, row: 1, column: 6, game_id: game.id)
+
+      expect(bishop.valid_move?(1, 3)).to eq false
+      expect(bishop.valid_move?(1, 7)).to eq false
+    end
+
+    it "should return false when moving vertical" do
+      game = FactoryGirl.create(:game)
+      bishop = FactoryGirl.create(:bishop, row: 1, column: 6, game_id: game.id)
+
+      expect(bishop.valid_move?(5, 6)).to eq false
+      expect(bishop.valid_move?(3, 6)).to eq false
+    end
+
+    it "should return false when moving outside of board" do
+      game = FactoryGirl.create(:game)
+      bishop = FactoryGirl.create(:bishop, row: 1, column: 6, game_id: game.id)
+
+      expect(bishop.valid_move?(4, 9)).to eq false
+      expect(bishop.valid_move?(0, 5)).to eq false
+    end
+  end
 end
