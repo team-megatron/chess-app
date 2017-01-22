80235bb0bb0525be353102a8d97b32fa255d3f4a
Ian Pan <ianpan@hotmail.com>
2017-01-22 04:21:59 +0000

    add test and logic if obstructed

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -2,24 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
   describe "valid_move?" do
+    it "should return FALSE if obstructed" do
+      game = FactoryGirl.create(:game)
+      rook = FactoryGirl.create(:rook, row: 1, column: 1, game_id: game.id)
+      piece = FactoryGirl.create(:piece, row: 1, column: 4, game_id: game.id)
+
+      expect(rook.valid_move?(1,7)).to eq false
+    end
+
     it "should return true for horizontal move" do
       game = FactoryGirl.create(:game)
       rook = FactoryGirl.create(:rook, row: 1, column: 1, game_id: game.id)
-      
+
       expect(rook.valid_move?(1,7)).to eq true
     end
-    
+
     it "should return true for a vertical move" do
       game = FactoryGirl.create(:game)
       rook = FactoryGirl.create(:rook, row: 1, column: 8, game_id: game.id)
-      
+
       expect(rook.valid_move?(7,8)).to eq true
     end
-    
+
     it "should return FALSE for non horizontal or vertical move" do
       game = FactoryGirl.create(:game)
       rook = FactoryGirl.create(:rook, row: 8, column: 8, game_id: game.id)
-      
+
       expect(rook.valid_move?(5,5)).to eq false
     end
   end
