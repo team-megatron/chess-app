34c584bae22fa7675fd30b091d1decccf9b5d06d
David Kreuzburg <dmk1991@gmail.com>
2017-02-01 23:26:01 +0000

    Cleans up tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,30 +3,25 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   describe "populate_game!" do
     before :each do
-
+      @game = FactoryGirl.create(:game)
+      @game.populate_game!
     end
 
     it "should create all necessary pieces for a game of chess" do
-      game = FactoryGirl.create(:game)
-      game.populate_game!
-      pieces = game.pieces
+      pieces = @game.pieces
 
       expect(pieces.length).to eq 32
     end
 
     it "should create 8 pawns of each color" do
-      game = FactoryGirl.create(:game)
-      game.populate_game!
-      pieces = game.pieces
+      pieces = @game.pieces
 
       expect(pieces.where(type: 'Pawn', is_black: false).count).to eq 8
       expect(pieces.where(type: 'Pawn', is_black: true).count).to eq 8
     end
 
     it "should create two rooks, knights, and bishops for each color" do
-      game = FactoryGirl.create(:game)
-      game.populate_game!
-      pieces = game.pieces
+      pieces = @game.pieces
 
       expect(pieces.where(type: 'Rook', is_black: false).count).to eq 2
       expect(pieces.where(type: 'Knight', is_black: false).count).to eq 2
@@ -38,9 +33,7 @@ RSpec.describe Game, type: :model do
     end
 
     it "should create a king and queen for each color" do
-      game = FactoryGirl.create(:game)
-      game.populate_game!
-      pieces = game.pieces
+      pieces = @game.pieces
 
       expect(pieces.where(type: 'Queen', is_black: false).count).to eq 1
       expect(pieces.where(type: 'Queen', is_black: true).count).to eq 1
