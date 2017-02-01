5682e647f5e98857ab2d709cf25555d0df6e21a3
David Kreuzburg <dmk1991@gmail.com>
2017-02-01 23:45:18 +0000

    adds empty methods

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,15 +1,22 @@
 class King < Piece
 
   def valid_move?(row_destination, col_destination)
-    
+
     row_diff = (self.row - row_destination).abs
     col_diff = (self.column - col_destination).abs
-    
+
     if (row_diff <= 1) && (col_diff <= 1)
       return true
     end
-      
-    return false 
+
+    return false
   end
-  
+
+  def can_castle?()
+
+  end
+
+  def castle(row_destination, col_destination)
+
+  end
 end
