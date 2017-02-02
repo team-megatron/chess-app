a36eca0c9e75a164980e74ab3495825aac941d45
David Kreuzburg <dmk1991@gmail.com>
2017-02-02 16:40:58 +0000

    Completes king castle method.

--- app/models/king.rb
+++ app/models/king.rb
@@ -4,6 +4,8 @@ class King < Piece
     # check if move to outside chessboard
     return false if !super
 
+    return true if can_castle?(row_destination, col_destination)
+
     row_diff = (self.row - row_destination).abs
     col_diff = (self.column - col_destination).abs
 
@@ -32,7 +34,25 @@ class King < Piece
     return true
   end
 
-  def castle(row_destination, col_destination)
+  # If a king is castling, do not call super
+  # If a king is not castling, call super
+  def move_to(row_destination, col_destination)
+    if can_castle?(row_destination, col_destination)
+      # Determine if the king is castling left or right
+      direction = col_destination > self.column ? 1 : -1
 
+      # Using direction, determine what row the king and rook belong on
+      king_column = self.column + (2 * direction)
+      rook_column = king_column + (1 * (direction * -1))
+
+      # Update the king and rook using these newly found columns
+      self.update_attributes(column: king_column)
+      self.game.pieces.active.find_by(
+        row: row_destination,
+        column: col_destination
+      ).update_attributes(column: rook_column)
+    else
+      super
+    end
   end
 end
