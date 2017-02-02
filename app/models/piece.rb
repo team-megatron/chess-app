9d78933da6e03655b985a7d0f42f03e9dc0a7697
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-02-02 13:22:10 +0000

    Add obstruction if there's a piece of same color at destination

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -17,6 +17,7 @@ class Piece < ActiveRecord::Base
   end
 
   def is_obstructed?(row_destination, col_destination)
+    return true if has_same_piece_color(row_destination, col_destination)
     return true if is_obstructed_horizontally?(row_destination, col_destination)
     return true if is_obstructed_vertically?(row_destination, col_destination)
     return true if is_obstructed_diagonally?(row_destination, col_destination)
