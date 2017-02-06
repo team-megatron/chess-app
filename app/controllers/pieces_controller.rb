23aa9571ae16c9dd92d6841c751c0306087505c0
Chuong Nguyen <chuong.v.nguyen@gmail.com>
2017-02-07 02:20:15 +1100

    Add turns checking, update chessboard remotely using Pusher

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -15,16 +15,54 @@ class PiecesController < ApplicationController
   end
 
   def update
-    if current_piece.valid_move?(params[:row].to_i, params[:column].to_i)
+    # check if current_player can make the move
+    # or selects the right piece color
+    # or the move is valid
+    if (current_player.id == active_player.id &&
+        current_piece.is_black == (active_player.id == black_player.id) &&
+        current_piece.valid_move?(params[:row].to_i, params[:column].to_i))
+      # save old position before moving
+      old_row = current_piece.row
+      old_column = current_piece.column
       current_piece.move_to(params[:row], params[:column])
+
+      # swap active player
+      if current_piece.game.active_player == current_piece.game.white_player
+        current_piece.game.update_attributes(active_player: current_piece.game.black_player)
+      else
+        current_piece.game.update_attributes(active_player: current_piece.game.white_player)
+      end
+
+      # inform of successful move
+      channel_name = 'game_channel_' + current_piece.game.id.to_s
+      puts channel_name
+      Pusher['game_channel_' + current_piece.game.id.to_s].trigger('move', {
+        :old_row => old_row, :old_column => old_row,
+        :new_row => params[:row], :new_column => params[:column],
+        :piece_id => current_piece.id
+      })
       render json: current_piece
+    else
+      render json: {}
     end
   end
 
   private
 
     def current_piece
-      @piece ||= Piece.find(params[:id])
+      @current_piece ||= Piece.find(params[:id])
+    end
+
+    def current_player
+      @current_player ||= Player.find(params[:current_player_id])
+    end
+
+    def active_player
+      @active_player ||= Player.find(current_piece.game.active_player_id)
+    end
+
+    def black_player
+      @black_player ||= current_piece.game.black_player
     end
 
     def is_actual_move?(row_destination, col_destination)
